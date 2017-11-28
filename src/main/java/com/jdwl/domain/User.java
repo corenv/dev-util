9b8fd0bc51b6d5eec3e75faac692863b2cf236e7
zhouwu18 <zhouwu@jd.com>
2017-11-28 13:58:31 +0800

    代码初始化

---
+++ src/main/java/com/jdwl/domain/User.java
@@ -0,0 +1,95 @@
+package com.jdwl.domain;
+
+import java.util.Date;
+
+public class User {
+    private Long id;
+
+    private String name;
+
+    private Long version;
+
+    private Boolean isDelete;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private String createUser;
+
+    private String updateUser;
+
+    private Date ts;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    public Boolean getIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(Boolean isDelete) {
+        this.isDelete = isDelete;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public String getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(String updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    public Date getTs() {
+        return ts;
+    }
+
+    public void setTs(Date ts) {
+        this.ts = ts;
+    }
+}
